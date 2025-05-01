4570120cbaa6799fb8fa0cad570310420a2a12dd
Justin Sovine <justinsovine@gmail.com>
2025-04-30 21:07:11 -0400

    Created a bug fixture

---
+++ api/tests/Fixture/BugsFixture.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * BugsFixture
+ */
+class BugsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'priority' => 'Lorem ipsum dolor sit amet',
+                'status' => 'Lorem ipsum dolor sit amet',
+                'created_at' => '2025-05-01 01:06:58',
+                'timestamp' => 'Lorem ipsum dolor sit amet',
+                'updated_at' => '2025-05-01 01:06:58',
+            ],
+        ];
+        parent::init();
+    }
+}
